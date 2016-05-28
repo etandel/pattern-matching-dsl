e8ee62e22ac88d75999d7b313c5fe7b1cf22512a
Elias Tandel Barrionovo <elias.tandel@gmail.com>
2016-05-28 11:10:57 -0300

    Bump version to 0.3.0

--- pattern/__init__.py
+++ pattern/__init__.py
@@ -2,4 +2,4 @@ from .core import Pattern, Any, Literal
 from .char_classes import CHAR, LETTER, UPPER, LOWER, DIGIT
 from .multiplier import Multiplier, WHATEVER, ONE_OR_MORE, Range, FixedWidth
 
-__version__ = '0.2.0'
+__version__ = '0.3.0'
